85132c33eb8fb71041e75c53f5dd5cd97dca70f5
Claudio Alessandro <claudioalessandro_22@hotmail.com>
2022-02-11 10:58:41 -0300

    primeiro commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"claudio.alessandro_22@hotmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
